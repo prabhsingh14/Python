90e8c82326cf13599a64db03e720b5487f5cc2bd
Prabh Singh <prabhsingh@Prabhs-MacBook-Pro.local>
2025-08-08 11:30:02 +0530

    Adding django files

---
+++ Django/djangocodes/djangocodes/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    #return HttpResponse("Hello world! This is home page")
+    return render(request, 'website/index.html')
+
+def about(request):
+    return HttpResponse("Hello world! This is about page")
---
+++ Django/djangocodes/projdjango/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProjdjangoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'projdjango'
